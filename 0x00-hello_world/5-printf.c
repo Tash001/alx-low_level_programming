4a879be59e41198afe5e83fe4f676831b54cec4f
tash001 <geofreykkibet@gmail.com>
2023-06-17 06:57:51 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that outputss using te printf
+ * main - A program that outputss using the printf function
  * Return: 0 (Successful)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
