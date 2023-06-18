6a184d5de460bca893d683d269611a884e4f4674
tash001 <geofreykkibet@gmail.com>
2023-06-18 05:07:26 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Beginning of the program
+ * Return: Always 0 (Succesfully)
+ */
+int main(void)
+{
+	char ch;
+	{
+	for (ch = 'z'; ch >= 'a'; ch++)
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
