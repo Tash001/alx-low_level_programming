3cfe4b14ae95a53228c1202dafc974d4d459d3c2
tash001 <geofreykkibet@gmail.com>
2023-06-18 03:18:15 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Beginning of the program
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+		putchar("\n");
+	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar("\n");
+	}
+	return (0);
+}
+
