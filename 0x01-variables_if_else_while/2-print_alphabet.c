742f8675992b550fbc326e8e9500f70fc63823dc
tash001 <geofreykkibet@gmail.com>
2023-06-18 02:11:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Successfully)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putcchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
