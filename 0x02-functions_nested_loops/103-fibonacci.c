6e6a95d7ec328f20a6020ba6cbf9d346dd8eed75
tash001 <geofreykkibet@gmail.com>
2023-06-21 12:39:00 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of fibonacci numbers
+ * less than four million
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int x = 0;
+	long y = 1, k = 2, sum = k;
+
+	while (k + y < 4000000)
+	{
+		k += y;
+		if (k % 2 == 0)
+		sum += k;
+		j = k - y;
+		++x;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
