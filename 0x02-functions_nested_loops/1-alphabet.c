ade4826fe85c274080a664b820423342342e5ffe
tash001 <geofreykkibet@gmail.com>
2023-06-21 01:29:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program that prints putchar
+ * Return: Always 0 (Successfully)
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch='a'; ch<='z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
+
