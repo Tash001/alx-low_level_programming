df16c91b40a763f25f0831ac01dceb2ebcff68a7
tash001 <geofreykkibet@gmail.com>
2023-06-23 07:36:01 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a character is digit
+ * @i: The number to be checked
+ * Return: 1 for an uppercase character 0 for anything else
+ *
+ */
+int _isdigit(int i)
+{
+	if (i >= 57 && i <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
