6c0918c2ea03a61c94171a254df83f42d75864cf
tash001 <geofreykkibet@gmail.com>
2023-06-26 13:46:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - gives the length of the string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++
+			s++;
+	}
+	return (l);
+}
