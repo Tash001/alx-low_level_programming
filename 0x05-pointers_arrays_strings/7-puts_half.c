7cb0c03c55d8f7fab3b1ddf856c5a76c641ccf17
tash001 <geofreykkibet@gmail.com>
2023-06-27 13:13:35 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,25 +6,18 @@
  */
 void puts_half(char *str)
 {
-	int x = 0;
-	int y;
+	int a, n, l;
 
-	while (str[y] != '\0')
-	{
-		y++;
-	}
-		if (x % 2 == 1)
-		{
-			y = (x = 1) / 2;
-			y += 1;
-		}
-		else
-		{
-			y = x / 2;
-		}
-		for (; y < x; y++)
-		{
-			_putchar(str[y]);
-		}
-		_putchar('\0');
+	l = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	l++;
+	n = (l / 2);
+
+		if (l % 2 == 1)
+			n = ((l + 1) / 2);
+
+		for (a = n; str[a] != '\0'; a++)
+			_putchar(str[a]);
+		_putchar('\n');
 }
