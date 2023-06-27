e227502f28ee941dbb96cac1f5383e696b13bc2a
tash001 <geofreykkibet@gmail.com>
2023-06-27 11:29:14 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 -prints every chacter of a string beginning with the first character
+ * @str: The string to be output
+ * Return: void always
+ */
+void puts2(char *str)
+{
+	int x;
+	int y = 0;
+
+	while (str[y] != '\0')
+	{
+		y++
+	}
+	for (x = 0; x < y; x += 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
