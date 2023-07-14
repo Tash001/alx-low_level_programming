d086dbd0cdb5b1a02a4fb41a1f832542083a492c
tash001 <geofreykkibet@gmail.com>
2023-07-14 06:44:37 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,8 +10,8 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
@@ -29,7 +29,7 @@ char *cap_string(char *str)
 		    index == 0)
 			str[index] -= 32;
 
-			index++;
+		index++;
 	}
 
 	return (str);
